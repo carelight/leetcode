f38c8bd92565cd54a26feddd7c764fb719a1504c
Anshul Upreti <108143494+carelight@users.noreply.github.com>
2022-11-18 18:48:22 +0530

    Time: 190 ms (47.02%), Space: 19.9 MB (85.25%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-        sort(nums.begin() , nums.end());    
-        if(nums.size() < 3){   
-            return {};
-        }
-        if(nums[0] > 0){        
-            return {};
-        }
-        vector<vector<int>> answer;
-        for(int i = 0 ; i < nums.size() ; ++i){     
-            if(nums[i] > 0){     
-                break;
-            }
-            if(i > 0 && nums[i] == nums[i - 1]){   
-                continue;
-            }
-            int low = i + 1 , high = nums.size() - 1;   
-            int sum = 0;
-            while(low < high){                       
-                sum = nums[i] + nums[low] + nums[high];
-                if(sum > 0){  
-                    high--;
-                } else if(sum < 0){ 
-                    low++;
-                } else {
-                    answer.push_back({nums[i] , nums[low] , nums[high]});  //we have found the required triplet, push it in answer vector
-                    int last_low_occurence = nums[low] , last_high_occurence = nums[high];  //Now again, to avoid duplicate triplets, we have to navigate to last occurences of num[low] and num[high] respectively
-                    while(low < high && nums[low] == last_low_occurence){   // Update the low and high with last occurences of low and high.
-                        low++;
-                    }
-                    while(low < high && nums[high] == last_high_occurence){
-                        high--;
-                    }
+            vector<vector<int>>ans;
+        sort(nums.begin(),nums.end());
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            
+            int a=nums[i];
+            int t=-a;
+            int s=i+1,e=n-1;
+            while(s<e){
+                if(nums[s]+nums[e]==t)
+                {        
+            ans.push_back({nums[i],nums[s],nums[e]});
+                    while(s<e && nums[e]==nums[e-1])e--;                    
+                 s++,e--;
                 }
+                else if(nums[s]+nums[e]>t)
+                {
+                    e--;
+                }
+                else s++;
             }
+            while(i+1<n && nums[i+1]==nums[i])i++;
         }
-        return answer;      //Return the answer vector.
+    return ans;
     }
 };
