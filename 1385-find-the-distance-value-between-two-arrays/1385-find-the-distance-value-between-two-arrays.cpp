4ec45f24ee88d98a08efcf6856adc447acb5f93a
Anshul Upreti <108143494+carelight@users.noreply.github.com>
2022-11-27 00:00:53 +0530

    Time: 31 ms (28.89%), Space: 13.2 MB (31.87%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,11 +1,17 @@
 class Solution {
 public:
-    int findTheDistanceValue(vector<int>& A, vector<int>& B, int d) {
-        sort(begin(B), end(B));
-        int ans = 0;
-        for (int n : A) {
-            if (upper_bound(begin(B), end(B), n + d) == lower_bound(begin(B), end(B), n - d)) ++ans;
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+            int cnt=0;
+        for(int i=0;i<arr1.size();i++){
+            bool f=false;
+            for(int j=0;j<arr2.size();j++){
+                if(abs(arr1[i]-arr2[j])<=d){
+                    f=true;
+                    break;
+                }
+            }
+            if(f==false) cnt++;
         }
-        return ans;
-    }
+        return cnt;
+    }  
 };
